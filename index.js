317af8d6f481b46378861fe125349ff14e84fc1a
Arkadiusz Sygulski <arkadiusz@sygulski.pl>
2018-01-03 13:50:04 +0100

    Update using latest node LTS magic

--- index.js
+++ index.js
@@ -1,88 +1,45 @@
-var request = require('request');
+const got = require('got');
+const map = require('p-map');
 
 /**
  * Retrieve price for single item.
  *
  * @param {number} appid - Steam application id
  * @param {String} name - Item name, market hashed
- * @param {Function} callback - Callback function(err, data)
  * @param {number} [currency=1] - Currency code number, default USD (https://github.com/SteamRE/SteamKit/blob/master/Resources/SteamLanguage/enums.steamd#L696)
  */
-exports.getItemPrice = function(appid, name, callback, currency) {
-    if (typeof callback !== 'function') {
-        throw new Error('No callback supplied');
+async function getItemPrice(appid, name, currency = 1) {
+  const response = await got('http://steamcommunity.com//market/priceoverview', {
+    json: true,
+    query: {
+      currency: currency,
+      appid: appid,
+      market_hash_name: name
     }
+  });
 
-    if (typeof currency !== 'number') {
-        currency = 1;
-    }
-
-    request({
-        uri: '/market/priceoverview',
-        baseUrl: 'http://steamcommunity.com/',
-        json: true,
-        qs: {
-            currency: currency,
-            appid: appid,
-            market_hash_name: name
-        }
-    }, function(err, response, body) {
-        if (!err && response.statusCode === 200) {
-            body.market_hash_name = name;
-            callback(null, body);
-        } else {
-            callback(err);
-        }
-    });
-};
-
+  return { ...response.body, market_hash_name: name };
+}
 
 /**
- * Retrieve price for list of items.
+ * Retrieve prices for list of items.
  *
  * @param {number} appid - Steam application id
- * @param {Object} names - Item name, market hashed
- * @param {Function} callback - Callback function(err, data)
+ * @param {Array} names - List of item names, market hashed
  * @param {number} [currency=1] - Currency code number, default USD (https://github.com/SteamRE/SteamKit/blob/master/Resources/SteamLanguage/enums.steamd#L696)
+ * @param {number} [concurrency=1] - Number of concurrently running requests
  */
-exports.getItemsPrice = function(appid, names, callback, currency) {
-    if (typeof callback !== 'function') {
-        throw new Error('No callback supplied');
-    }
+async function getItemsPrices(appid, names, currency = 1, concurrency = 1) {
+  if (!Array.isArray(names)) {
+    if (typeof names === 'string') return getItemsPrices(appid, [names], currency);
+    throw new Error(`Invalid argument names, expected array got ${typeof names}`);
+  }
 
-    if (typeof currency !== 'number') {
-        currency = 1;
-    }
+  const result = {};
+  const items = await map(names, name => getItemPrice(appid, name, currency).catch(err => ({ success: false, market_hash_name: name, err })), { concurrency });
+  items.forEach(item => result[item.market_hash_name] = item);
 
-    if (typeof names !== 'object') {
-        if (typeof names === 'string') {
-            names = [names];
-        } else {
-            throw new Error('Non-object supplied');
-        }
-    }
+  return result;
+}
 
-    var result = {};
-
-    names.forEach(function(name) {
-        request({
-            uri: '/market/priceoverview',
-            baseUrl: 'http://steamcommunity.com/',
-            json: true,
-            qs: {
-                currency: currency,
-                appid: appid,
-                market_hash_name: name
-            }
-        }, function(err, response, body) {
-            if (!err && response.statusCode === 200) {
-                result[name] = body;
-            } else {
-                result[name] = {'success': false};
-            }
-            if(Object.keys(result).length === names.length) {
-                callback(result);
-            }
-        });
-    });
-};
+module.exports = { getItemPrice, getItemsPrices };
